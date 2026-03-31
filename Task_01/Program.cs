57cb6745d71d0887d18e80da37c9e85d51308d29
maximponomarenko-art <maximponomarenko1111@gmail.com>
2026-03-31 23:08:28 +0300

    Added 2 lab programs

---
+++ Task_01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введіть перше число:");
+            double arg1 = double.Parse(Console.ReadLine());
+            Console.Write("Введіть друге число:");
+            double arg2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(
+                $"{arg1}+{arg2}={arg1+arg2}" +
+                $"\n{arg1}-{arg2}={arg1 - arg2}" +
+                $"\n{arg1}*{arg2}={arg1 * arg2}" +
+                $"\n{arg1}/{arg2}={arg1 / arg2}" +
+                $"\n{arg1}%{arg2}={arg1 % arg2}");
+
+        }
+    }
+}
---
+++ Task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Оберіть операцію(1-'+',2-'-',3-'*',4-'/',5-'%'.):");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Введіть перше число:");
+            double arg1 = double.Parse(Console.ReadLine());
+            Console.Write("Введіть друге число:");
+            double arg2 = double.Parse(Console.ReadLine());
+            if (num == 1)Console.WriteLine($"{arg1}+{arg2}={arg1+arg2}");
+            if (num == 2) Console.WriteLine($"{arg1}-{arg2}={arg1 - arg2}");
+            if (num == 3) Console.WriteLine($"{arg1}*{arg2}={arg1 * arg2}");
+            if (num == 4) Console.WriteLine($"{arg1}/{arg2}={arg1 / arg2}");
+            if (num == 5) Console.WriteLine($"{arg1}%{arg2}={arg1 % arg2}");
+        }
+    }
+}
